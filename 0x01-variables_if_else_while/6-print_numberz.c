856a1c0f82f1c3f2228e8c1b3ed978225ddf9431
achrxf <achrxf@hotmail.com>
2023-09-08 16:48:41 +0100

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 10
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
