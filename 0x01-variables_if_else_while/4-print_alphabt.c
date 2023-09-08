fe27859e3c467d073d78211a5b943d04c86372ce
achrxf <achrxf@hotmail.com>
2023-09-08 16:35:58 +0100

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lowercase alphabet
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char i = 97;
+
+	while (i < 122)
+	{
+		if (i == 101 || i == 113)
+		{
+			i++;
+		}
+		else
+		{
+			putchar(i);
+			i++;
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
