c6873e98f09dcb1f3a5a7e90c52e24dbefd4c675
achrxf <achrxf@hotmail.com>
2023-09-08 16:41:15 +0100

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers from 0 to 10
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
