f9a1521dbc4e0c6dd35fb96fd2df3e8370e784be
achrxf <achrxf@hotmail.com>
2023-09-28 12:26:59 +0100

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - A function that returns the value of x pow y.
+ * @x: int type
+ * @y: int type
+ * Return: int
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+
+}
