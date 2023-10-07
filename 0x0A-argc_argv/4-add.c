8a64096a8e88cdbbcaa8a557d4001a358fbba46c
achrxf <achrxf@hotmail.com>
2023-10-07 12:22:53 +0100

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * integer - checks for int
+ * @p: char
+ * Return: 0
+ */
+int integer(char *p)
+{
+	int i;
+
+	for (i = 0; p[i] != '\0'; i++)
+	{
+		if (p[i] < 48 || p[i] > 57)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+
+/**
+ * main - multiplies two numbers.
+ * @argc: int
+ * @argv: char
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int res, i, conv;
+
+	if (argc < 3)
+	{
+		printf("0\n");
+		exit(-1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		conv = atoi(argv[i]);
+		if (conv < 0 || integer(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			res += conv;
+		}
+	}
+	printf("%d\n", res);
+	return (0);
+}
