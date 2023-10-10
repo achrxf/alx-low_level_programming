12bf16877039346b7da1d9381c91ea9d636af7a8
achrxf <achrxf@hotmail.com>
2023-10-10 15:35:56 +0100

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog defining
+ * @name: char
+ * @age: float
+ * @owner: char
+*/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
